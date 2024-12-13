960f624c4180802df73efbc8172a47e807d3f3e8
Chanbolone <chanbolone@gmail.com>
2024-12-12 19:32:21 -0500

    second

--- script.js
+++
@@ -1,6 +0,0 @@
-// Add event listener to the contact form
-document.querySelector('form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    // Send the form data to your server or email
-    console.log('Form submitted');
-});
